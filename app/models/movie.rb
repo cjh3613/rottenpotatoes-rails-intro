fe39b90729f4af183eba0d9f498981693fae10c4
EC2 Default User <ec2-user@ip-172-31-13-38.us-east-2.compute.internal>
2021-02-21 07:27:33 +0000

    version

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,9 +1,9 @@
 class Movie < ActiveRecord::Base
-   def self.all_ratings
+    def self.all_ratings
        return pluck(:rating).uniq
     end
-    
-   def self.accompany_ratings(ratings)
+
+    def self.accompany_ratings(ratings)
        where(rating: ratings.keys)
     end
 end

c67b07033ebfccc928937eaea1d6dbebc846b7e1
EC2 Default User <ec2-user@ip-172-31-13-38.us-east-2.compute.internal>
2021-02-21 07:23:46 +0000

    final version

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,7 +7,7 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @all_ratings = ['G','PG','PG-13','R']
+    @all_ratings = Movie.all_ratings
     @movie_ratings = params[:ratings] || session[:ratings] || Hash[@all_ratings.map {|rating| [rating, 1]}]
     
     sort = params[:sort] || session[:sort]
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,8 @@
 class Movie < ActiveRecord::Base
+   def self.all_ratings
+       return pluck(:rating).uniq
+    end
+    
    def self.accompany_ratings(ratings)
        where(rating: ratings.keys)
     end
